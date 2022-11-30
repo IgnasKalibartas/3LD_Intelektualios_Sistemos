b1c3d08f23b4ffdaa10005e10b7f757ed6e7c98d
ignkal <ignkal@gmail.com>
2022-11-30 11:33:29 +0200

    [UPD] Done

--- laboratorinis_3.m
+++ laboratorinis_3.m
@@ -13,7 +13,30 @@ r2 = 0.16;
 %Gauso f-jos: F = exp(-(x-c)^2/(2*r^2));
 
 for i = 1:20
-    F1(i) = exp(-(x(i)-c1)^2/(2*r1^2));
-    F2(i) = exp(-(x(i)-c2)^2/(2*r2^2));
+    F1 = exp(-(x(i)-c1)^2/(2*r1^2));
+    F2 = exp(-(x(i)-c2)^2/(2*r2^2));
 end
-plot(x,F1, x,F2,x,y)
+
+% 2) Sugeneruojami atsitiktiniai svoriai. Pradines mokymo vertes.
+w1 = randn(1);
+w2 = randn(1);
+w0 = randn(1);
+n = 0.15;
+
+for m = 1:10000
+    for j = 1:20
+        y_apsk(j) = F1*w1+F2*w2+w0;
+        e = y(j) - y_apsk(j);
+        %atnaujinami svoriai
+        if e ~= 0
+            w1 = w1+n*e*F1;
+            w2 = w2+n*e*F2;
+            w0 = w0+n*e;
+        end
+    end
+end
+
+plot(x, y_apsk,x,y,'r')
+
+
+
