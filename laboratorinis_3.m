2ef849e836834cc8361155e55c746c6cac68d690
ignkal <ignkal@gmail.com>
2022-11-30 10:19:41 +0200

    [UPD] parinkti +- geri spinduliai ir centro taskas

--- laboratorinis_3.m
+++ laboratorinis_3.m
@@ -3,16 +3,17 @@ clc
 
 x = 0.1:1/22:1;
 y = (1 + 0.6*sin(2*pi*x/0.7)) + 0.3*sin(2*pi*x)/2;
-plot(x,y)
+%plot(x,y)
 % pasirinkti centrai
-c1 = 0.19;
-c2 = 0.9;
-r1 = 0.13;
-r2 = 0.17;
+c1 = 0.20;
+c2 = 0.89;
+r1 = 0.15;
+r2 = 0.16;
 
 %Gauso f-jos: F = exp(-(x-c)^2/(2*r^2));
 
 for i = 1:20
-    F1 = exp(-(x(i)-c1)^2/(2*r1^2));
-    F2 = exp(-(x(i)-c2)^2/(2*r2^2));
+    F1(i) = exp(-(x(i)-c1)^2/(2*r1^2));
+    F2(i) = exp(-(x(i)-c2)^2/(2*r2^2));
 end
+plot(x,F1, x,F2,x,y)
